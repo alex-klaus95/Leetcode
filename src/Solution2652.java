c9fc2f2babfc573e47e11ac842892c0e76861f79
alex-klaus95 <alex_klaus95@t-online.de>
2025-07-11 18:02:31 +0200

    test3

--- src/Solution2652.java
+++ src/Solution2652.java
@@ -13,34 +13,10 @@ public class Solution2652 {
 		}else {
 		return sum;
 	}
+    }
 	
-    	
-    }
-    
-    
-    class Solution {
-        public int sumOfMultiples(int n) {
-                
-        	int sum = 0;
-        		for(int i = 0; i <= n; i++) {
-        			if(i % 7 == 0 || i % 5 == 0|| i% 3 == 0) {
-        			    sum += i;
-        			}
-        		}
-        	
-        	return sum;
-        }
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
     
     
     public static void main(String[] args) {
